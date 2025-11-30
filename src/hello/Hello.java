1d78e2d8fe02e0f547284a434a952b57cb7efbaa
GOLLA SRIRAM YADAV <srh3052004@gmail.com>
2025-11-30 19:46:04 +0530

    Update output message in Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,6 +1,6 @@
 package hello;
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Pipeline CI/CD webhook!");
+        System.out.println("HelloJenkins Pipeline CI/CD webhook!");
     }
 }
